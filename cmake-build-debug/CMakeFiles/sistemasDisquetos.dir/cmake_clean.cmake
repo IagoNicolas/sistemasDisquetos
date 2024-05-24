9ddadc94d0667618cf3b7ae7f06a34c04be558b0
IagoNicolas <iago.nicolas@ufpe.br>
2024-05-23 22:08:53 -0300

    Added latex project

---
+++ cmake-build-debug/CMakeFiles/sistemasDisquetos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sistemasDisquetos.dir/main.c.o"
+  "CMakeFiles/sistemasDisquetos.dir/main.c.o.d"
+  "sistemasDisquetos"
+  "sistemasDisquetos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sistemasDisquetos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sistemasDisquetos.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sistemasDisquetos.
+# This may be replaced when dependencies are built.
